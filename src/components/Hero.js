41b9cbd613a956b4f85c8bf90acae1797ea4cd71
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-19 10:45:20 +0100

    make Hero component more responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,16 +4,18 @@ import '../App.css'
 
 function Hero() {
     return (
-        <div className="hero-container">
-            <div className="hero-container__content">
-                <h1>Bring everyone together to build better products.</h1>
-                <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
-                <a href="#" className="cta-button">Get Started</a>
+        <section class="hero">
+            <div className="hero-container">
+                <div className="hero-container__content">
+                    <h1>Bring everyone together to build better products.</h1>
+                    <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                    <a href="#" className="cta-button">Get Started</a>
+                </div>
+                <div className="hero-container__illustration">
+                    <img src={illustration} alt="" />
+                </div>
             </div>
-            <div className="hero-container__illustration">
-                <img src={illustration} alt="" />
-            </div>
-      </div>
+        </section>
     )
 }
 
