655f628df768e41f9a40cc34371e86b70111ae2a
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-19 10:36:38 +0100

    import all components to app.js component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Different from './components/Different'
+import Testimony from './components/Testimony'
+import Cta from './components/Cta'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Different />
+      <Testimony />
+      <Cta />
+      <Footer />
+    </>
   );
 }
 
